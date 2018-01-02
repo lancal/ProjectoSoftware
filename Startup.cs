0b060bacbaafe3e01a23e563452aa0280094d3c5
Alan <ivda.calderon@gmail.com>
2018-01-02 00:06:56 -0300

    starup class modified

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+
 namespace Cotizaciones
 {
     public class Startup
@@ -22,6 +23,7 @@ namespace Cotizaciones
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<CotizacionesContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
